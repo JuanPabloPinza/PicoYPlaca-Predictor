1058d0e711f5e989dd4d0a42915620cbebd1740b
Juan Pablo Pinza Armijos <pinza153@hotmail.com>
2026-06-21 13:35:12 -0500

    build - needed resources for backend

---
+++ src/main/java/git/jppinza/pico_y_placa_predictor_backend/PicoYPlacaPredictorBackendApplication.java
@@ -0,0 +1,13 @@
+package git.jppinza.pico_y_placa_predictor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicoYPlacaPredictorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicoYPlacaPredictorBackendApplication.class, args);
+	}
+
+}
