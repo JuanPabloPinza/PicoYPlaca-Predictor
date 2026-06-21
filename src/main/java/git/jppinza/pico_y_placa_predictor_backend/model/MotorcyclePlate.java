262a8d2d211d07e132c7751f87f084431c07f453
Juan Pablo Pinza Armijos <pinza153@hotmail.com>
2026-06-21 13:27:36 -0500

    refactor - Validations on the motorcycle plate

--- src/main/java/git/jppinza/pico_y_placa_predictor_backend/model/MotorcyclePlate.java
+++ src/main/java/git/jppinza/pico_y_placa_predictor_backend/model/MotorcyclePlate.java
@@ -8,14 +8,23 @@ public class MotorcyclePlate extends Plate {
 
     @Override
     public int getLastDigit() {
-        // Motorcycle logic: 2Letters + 3Numbers + 1LastLetter (e.g., PB341C)
-        // The validator digit is the last number before the final letter.
-        for (int i = plateNumber.length() - 1; i >= 0; i--) {
-            if (Character.isDigit(plateNumber.charAt(i))) {
-                return Character.getNumericValue(plateNumber.charAt(i));
-            }
+        // 1. A motorcycle plate must be 6 characters long (2letters + 3numbers + 1letter)
+        if (plateNumber.length() != 6) {
+            throw new IllegalArgumentException("Invalid Motorcycle Plate");
         }
-        throw new IllegalArgumentException("Invalid Motorcycle Plate!");
+        // 2. Las character must be a letter
+        char lastChar = plateNumber.charAt(5);
+        if (!Character.isLetter(lastChar)) {
+            throw new IllegalArgumentException("Invalid Motorcycle Plate");
+        }
+        
+        // 3. The character right before the last letter MUST be our validator number (index 4)
+        char validatorDigit = plateNumber.charAt(4);
+        if (!Character.isDigit(validatorDigit)) {
+            throw new IllegalArgumentException("Invalid Motorcycle Plate");
+        }
+        
+        return Character.getNumericValue(validatorDigit);
     }
 
     @Override
